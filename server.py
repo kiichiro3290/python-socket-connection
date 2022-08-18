47587f11f6ac9d1c0cf44b5e017bb48305a8e8a8
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-08-18 15:38:34 +0900

    update server.py

--- server.py
+++ server.py
@@ -1,21 +1,30 @@
 #server.py
 import socket
+import csv
+import datetime
 
 ip_address = 'サーバー側のIPアドレス'
 port = 7010
 buffer_size = 4092
 
-# Socketの作成
+# create the socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    # IP Adress とPort番号をソケット割り当てる
+    # assing the IP Adress and the Port number
     s.bind((ip_address, port))
-    # Socketの待機状態
+    # creating socket
     s.listen(5)
-    # while Trueでクライアントからの要求を待つ
-    while True:
-        # 要求があれば接続の確立とアドレス、アドレスを代入
-        conn, addr = s.accept()
+    datetime_str = str(datetime.datetime.now().strftime('%Y%m%d%H%M'))
+    with open(f'../data/{datetime_str}_link1.csv', 'w') as f:
+        writer=csv.writer(f)
+        # wait for client sending the socket
         while True:
-            # データを受信する
-            data = conn.recv(buffer_size)
-            print(data.decode())
+            # 要求があれば接続の確立とアドレス、アドレスを代入
+            # establish client/server communication
+            conn, addr = s.accept()
+            while True:
+                # receive the data
+                data = conn.recv(buffer_size)
+                # write csv file
+                csvData = list(data.decode().split(','))
+                writer.writerow(csvData)
+                print(data.decode())
