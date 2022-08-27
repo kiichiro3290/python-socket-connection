f2b16599a543720a31e29fe8cafdc32a56401365
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-08-27 11:23:57 +0900

    add convert_txt_to_csv.py

---
+++ convert_txt_to_csv.py
@@ -0,0 +1,21 @@
+import sys
+import csv
+
+args = sys.argv
+
+print(args[0])
+print("inputfile:" + args[1])
+print("outputfie:" + args[2])
+
+f1 = open(args[1])
+lines = f1.readlines()
+for l in lines:
+    print(l)
+    with open(args[2], "w") as f:
+        for l in lines:
+
+            row=list(l.split(','))
+            writer=csv.writer(f)
+            writer.writerow(row)
+            print(row)
+
